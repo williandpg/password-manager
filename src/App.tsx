4a7f9f24a177fc9cfc36d2915cf378a137cda54d
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-06 14:50:01 -0300

    Requisito 1 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <header>
+        <Title />
+      </header>
+      <div>
+        Hello World
+      </div>
+    </>
   );
 }
 
