88143918c9b5657e3371b1f778513f4a5626cde5
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-06 19:06:19 -0300

    Requisito 4 iniciado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,60 @@
+import React, { useState } from 'react';
+
 type Props = {
   buttonClose: (parametro: boolean) => void;
 };
 
 function Form(props:Props) {
   const { buttonClose } = props;
+  const [formValues, setFormValues] = useState({
+    name: '',
+    login: '',
+    password: '',
+    url: '',
+  });
+
+  const handleInputChange = (event: { target: { id: any; value: any; }; }) => {
+    const { id, value } = event.target;
+    setFormValues({ ...formValues, [id]: value });
+  };
+
+  const isButtonDisabled = !(formValues.name && formValues.login && formValues.password && formValues.password.length >= 8 && formValues.password.length <= 16 && /\d/.test(formValues.password) && /[a-zA-Z]/.test(formValues.password) && /[^a-zA-Z0-9]/.test(formValues.password));
 
   return (
     <form action="">
       <label htmlFor="name">Nome do serviço</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        id="name"
+        value={ formValues.name }
+        onChange={ handleInputChange }
+      />
 
       <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
+      <input
+        type="text"
+        id="login"
+        value={ formValues.login }
+        onChange={ handleInputChange }
+      />
 
       <label htmlFor="password">Senha</label>
-      <input type="password" id="password" />
+      <input
+        type="password"
+        id="password"
+        value={ formValues.password }
+        onChange={ handleInputChange }
+      />
 
       <label htmlFor="url">URL</label>
-      <input type="text" id="url" />
+      <input
+        type="text"
+        id="url"
+        value={ formValues.url }
+        onChange={ handleInputChange }
+      />
 
-      <input type="button" value="Cadastrar" />
+      <input type="button" value="Cadastrar" disabled={ isButtonDisabled } />
       <input onClick={ () => { buttonClose(true); } } type="button" value="Cancelar" />
     </form>
   );
