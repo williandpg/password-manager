3b9135ee3395e11acbd4b2947d64eca006d94615
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-07 18:32:02 -0300

    Requisito 5 em andamento

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -15,8 +15,28 @@ function Form(props:Props) {
   const handleInputChange = (event: { target: { id: any; value: any; }; }) => {
     const { id, value } = event.target;
     setFormValues({ ...formValues, [id]: value });
+    passwordVerification(value);
   };
   const isButtonDisabled = !(formValues.name && formValues.login && formValues.password && formValues.password.length >= 8 && formValues.password.length <= 16 && /\d/.test(formValues.password) && /[a-zA-Z]/.test(formValues.password) && /[^a-zA-Z0-9]/.test(formValues.password));
+
+  const [verificationPassword, setVerificationPassword] = useState({
+    caracMax: false,
+    caracMin: false,
+    numberAndLetter: false,
+    specialCarac: false,
+  });
+  const passwordVerification = (password: string) => {
+    const caracMax = password.length <= 16;
+    const caracMin = password.length >= 8;
+    const numberAndLetter = /\d/.test(password) && /[a-zA-Z]/.test(password);
+    const specialCarac = /[^a-zA-Z0-9]/.test(password);
+    setVerificationPassword({
+      caracMax,
+      caracMin,
+      numberAndLetter,
+      specialCarac,
+    });
+  };
 
   return (
     <form action="">
@@ -41,6 +61,20 @@ function Form(props:Props) {
         value={ formValues.password }
         onChange={ handleInputChange }
       />
+
+      <label className={ verificationPassword.caracMin ? 'valid-password-check' : 'invalid-password-check' }>
+        Possuir 8 ou mais caracteres
+      </label>
+      <label className={ verificationPassword.caracMax ? 'valid-password-check' : 'invalid-password-check' }>
+        Possuir até 16 caracteres
+      </label>
+      <label className={ verificationPassword.numberAndLetter ? 'valid-password-check' : 'invalid-password-check' }>
+        Possuir letras e números
+      </label>
+      <label className={ verificationPassword.specialCarac ? 'valid-password-check' : 'invalid-password-check' }>
+        Possuir algum caractere especial
+      </label>
+
       <label htmlFor="url">URL</label>
       <input
         type="text"
